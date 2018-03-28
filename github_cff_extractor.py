057ce65c40edb7963b736536f566da586743c4f8
David McKain <david.mckain@ed.ac.uk>
2018-03-28 14:13:48 +0100

    Minor code tidy. Initial API is now roughly stable.

--- github_cff_extractor.py
+++ github_cff_extractor.py
@@ -13,12 +13,12 @@ with open(auth_token_file, 'r') as f:
     auth_token = f.read().strip()
 
 # TODO: These will become user-selectable inputs
-input_username = 'codemeta'
-input_reponame = 'codemeta'
+# input_username = 'codemeta'
+# input_reponame = 'codemeta'
 
 # This has releases + a newer tag!
 # Here we'd expect to take the last release rather than the last branch
-#input_username = 'citation-file-format'
+#input_username = 'citation-file-to_cff_string'
 #input_reponame = 'cff-converter-python'
 
 # This has no releases, but lots of tags
@@ -26,46 +26,40 @@ input_reponame = 'codemeta'
 #input_reponame = 'jacomax'
 
 # This has no releases, no tags, just a branch
-#input_username = 'davemckain'
-#input_reponame = 'asciimath-parser'
+input_username = 'davemckain'
+input_reponame = 'asciimath-parser'
 
 ###########################################################
 
 
 def _debug_list(name, paginated_list):
-    '''Temporary to debug the contents of a PagninatedList object'''
+    """Temporary to debug the contents of a PagninatedList object"""
     print("Debugging {}".format(name))
     for item in paginated_list:
         pprint(item)
 
 
-class CffAuthor:
-    """
-    FIXME: Downgrade this to a dictionary, as that's what we're returning elsewhere
-    """
-    def __init__(self, family_names, given_names, entity_name=None):
-        self.family_names = family_names
-        self.given_names = given_names
-        self.entity_name = entity_name
-
-    def format(self):
-        if self.entity_name:
-            return 'name: {}'.format(self.entity_name)
-        else:
-            return 'family-names: {}\ngiven-names: {}'.format(
-                self.family_names, self.given_names
-            )
-
-
 class GitHubCffGuesser:
+    """
+    This class extracts (guesses?) CFF metadata from a Github repository
+    object.
+    """
+
     def __init__(self, repo):
         self.repo = repo
         self.parsed = None
 
     def run(self):
+        """
+        This method performs the data extraction, storing the results within
+        this Object.
+
+        There's no real need to call this explicitly as it will be called lazily
+        via output methods.
+        """
         result = dict()
         result['title'] = self.repo.name
-        result['contributors'] = self._extract_contributors()
+        result['authors'] = self._extract_contributors()
         self._extract_release_info(result)
         self.parsed = result
 
@@ -100,14 +94,24 @@ class GitHubCffGuesser:
         return ' '.join(bits[0:-1]), bits[-1]
 
     def _extract_contributors(self):
+        """
+        Extracts contributor information from this repository
+        """
         result = []
         for contributor in self.repo.get_contributors():
             name = contributor.name
             if contributor.type == 'User':
+                # This is a real person
                 first, last = self._parse_human_name(name)
-                result.append(CffAuthor(first, last, None))
+                result.append({
+                    'family_names': last,
+                    'given_names': first,
+                })
             else:
-                result.append(CffAuthor(None, None, name))
+                # It's an organisation (I think...)
+                result.append({
+                    'name': name
+                })
         return result
 
     def to_cff_date(self, datetime):
@@ -121,10 +125,19 @@ class GitHubCffGuesser:
             self.run()
 
     def as_dict(self):
+        """
+        Returns a dictionary of extracted CFF metadata.
+
+        Keys agree with the names in CFF, except that '-' characters have
+        been converted to '_'.
+        """
         self._ensure_run()
         return self.parsed
 
-    def format(self):
+    def to_cff_string(self):
+        """
+        This returns a CFF string representation of the parsed data
+        """
         parsed = self.as_dict()
         return '''
 cff-version: 1.0.3
@@ -142,19 +155,22 @@ repository-code: {repo_url}'''.format(
 
     def _format_authors(self):
         formatted_items = []
-        for a in self.as_dict()['contributors']:
-            if a.entity_name:
-                formatted_items.append('  - name: {}\n'.format(a.entity_name))
+        for a in self.as_dict()['authors']:
+            if 'name' in a:
+                # It's an organisation
+                formatted_items.append('  - name: {}\n'.format(
+                    a['name']))
             else:
+                # It's a person
                 formatted_items.append(
-                    '  - family-names: {}\n    given-names: {}\n'.format(a.family_names, a.given_names))
+                    '  - family-names: {}\n    given-names: {}\n'.format(
+                        a['family_names'], a['given_names']))
         return 'authors:\n' + ''.join(formatted_items)
 
 
-g = Github(auth_token)
-u = g.get_user(input_username)
-repo = u.get_repo(input_reponame)
-g = GitHubCffGuesser(repo)
-print(g.format())
-
-
+# Example code
+github = Github(auth_token)
+user = github.get_user(input_username)
+repo = user.get_repo(input_reponame)
+github2cff = GitHubCffGuesser(repo)
+print(github2cff.to_cff_string())
