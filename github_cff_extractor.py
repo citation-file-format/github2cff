c3aa89c25fc0304cee8f29d71cbd2c08c81aa3ab
David McKain <david.mckain@ed.ac.uk>
2018-03-30 14:52:26 +0100

    Minor tidy-up after initial work

--- github_cff_extractor.py
+++ github_cff_extractor.py
@@ -3,13 +3,6 @@ import re
 from github import Github
 
 
-# NOTE: Put the GitHub auth token in the file below for now. Do NOT commit this file to git :-)
-auth_token_file = 'auth_token.txt'
-
-with open(auth_token_file, 'r') as f:
-    auth_token = f.read().strip()
-
-
 class GitHubCffGuesser:
     """
     This class extracts (guesses?) CFF metadata from a Github repository
@@ -31,6 +24,16 @@ class GitHubCffGuesser:
         result = dict()
         result['title'] = self.repo.name
         result['authors'] = self._extract_contributors()
+        result['repository_code'] = self.repo.html_url
+
+        # Extract license data. This seems only to exist in the underlying raw data
+        repo_raw = self.repo.raw_data
+        license_raw = repo_raw['license']
+        if license_raw['spdx_id']:
+            result['license'] = license_raw['spdx_id']
+        if license_raw['url']:
+            result['license-url'] = license_raw['url']
+
         self._extract_release_info(result)
         self.parsed = result
 
@@ -52,7 +55,8 @@ class GitHubCffGuesser:
             result['date_released'] = self.to_cff_date(latest_tag.commit.commit.author.date)
         else:
             # No tags yet, so we'll take the last commit on the default branch
-            branch = repo.get_branch(repo.default_branch)
+            assert self.repo.default_branch  # TODO: Check this!
+            branch = self.repo.get_branch(self.repo.default_branch)
             result['version'] = branch.commit.sha
             result['date_released'] = self.to_cff_date(branch.commit.commit.author.date)
 
@@ -113,21 +117,27 @@ class GitHubCffGuesser:
     def to_cff_string(self):
         """
         This returns a CFF string representation of the parsed data
+
+        TODO: This is tedious and messy. Can't we use a YAML dump instead?
         """
         parsed = self.as_dict()
         return '''
 cff-version: 1.0.3
 message: If you use this software, please cite it as below.        
 title: {title}
+abstract: {abstract}
 version: {version}
 {authors_formatted}date-released: {date_released}
-repository-code: {repo_url}'''.format(
+repository-code: {repository_code}'''.format(
             title=parsed['title'],
+            abstract=self.repo.description,
             version=parsed['version'],
             authors_formatted=self._format_authors(),
             date_released=parsed['date_released'],
-            repo_url=self.repo.html_url,
-                   )
+            repository_code=parsed['repository_code'],
+        )
+        # TODO: Need to include license stuff.
+        return result
 
     def _format_authors(self):
         formatted_items = []
@@ -146,13 +156,20 @@ repository-code: {repo_url}'''.format(
 
 # Example code
 if __name__ == '__main__':
+
+    # NOTE: Put the GitHub auth token in the file below for now. Do NOT commit this file to git :-)
+    auth_token_file = 'auth_token.txt'
+
+    with open(auth_token_file, 'r') as f:
+        auth_token = f.read().strip()
+
     # TODO: These would normally be user-selectable inputs
     input_username = 'codemeta'
     input_reponame = 'codemeta'
 
     # This has releases + a newer tag!
     # Here we'd expect to take the last release rather than the last branch
-    #input_username = 'citation-file-to_cff_string'
+    #input_username = 'citation-file-format'
     #input_reponame = 'cff-converter-python'
 
     # This has no releases, but lots of tags
